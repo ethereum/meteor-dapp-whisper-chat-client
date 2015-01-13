ff314ad45844844be42161edc373f6329db4ad9c
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2015-01-13 20:52:09 -0200

    Added fonts and toolbar

--- app/client/routes.js
+++ app/client/routes.js
@@ -11,6 +11,7 @@ Router.route('/', function () {
     
     this.render('views_chats');
     this.render('views_chats_aside', {to: 'aside'});
+    this.render('views_actionbar', {to: 'actionbar'});
 });
 
 // Router.route('/items', function () {
