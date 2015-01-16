3838cc84af2f0a4a63bfafe4e821f45dcda56ae0
Fabian Vogelsteller <fabian@frozeman.de>
2015-01-16 11:00:11 +0100

    add minor changes

--- app/client/routes.js
+++ app/client/routes.js
@@ -11,7 +11,7 @@ Router.route('/', function () {
     
     this.render('views_chats');
     this.render('views_chats_aside', {to: 'aside'});
-    this.render('elements_actionbar', {to: 'actionbar'});
+    this.render('views_chats_actionbar', {to: 'actionbar'});
 });
 
 // Router.route('/items', function () {
