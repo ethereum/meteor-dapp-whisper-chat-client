53f0da756bae8da9f1b38d8347e7b38a36963655
Fabian Vogelsteller <fabian@frozeman.de>
2015-01-19 18:53:20 +0100

    add modals

--- app/client/templates/elements/modal.js
+++ app/client/templates/elements/modal.js
@@ -22,18 +22,19 @@ Template['elements_modal'].rendered = function(){
 
     // initiate the geo pattern
     var pattern = GeoPattern.generate(Math.random().toString());
-    $('.whisper-profile .p-nickname').css('background-image', pattern.toDataUrl());
-    $('.u-photo').attr("src", "http://www.gravatar.com/avatar/" + pattern.hash + '?d=retro&s=128')
+    $('.dapp-modal-header').css('background-image', pattern.toDataUrl());
+    $('.dapp-profile-image').css("background-image", "url(http://www.gravatar.com/avatar/" + pattern.hash + '?d=retro&s=128)');
 
     if (typeof chance != 'undefined') { 
         var genchance = new Chance(pattern.hash.toString);
-        $('.p-nickname').text(chance.prefix() + " " +  chance.capitalize(chance.word())); 
+        $('.dapp-unauthenticated').text(chance.prefix() + " " +  chance.capitalize(chance.word())); 
     }
 };
 
 
 Template['elements_modal'].events({
-    'click .dapp-modal-overlay': function(){
-        history.back();
+    'click .dapp-modal-overlay': function(e){
+        if($(e.target).hasClass('dapp-modal-overlay'))
+            history.back();
     }
 });
