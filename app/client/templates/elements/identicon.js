8cceca6ed7cb39cda8bbbaeb8b5b874f8c0a45f0
Fabian Vogelsteller <fabian@frozeman.de>
2015-02-06 14:54:52 +0100

    add latest version of app styles, change profileImage

---
+++ app/client/templates/elements/identicon.js
@@ -0,0 +1,24 @@
+/**
+Template Controllers
+
+@module Templates
+*/
+
+/**
+Return the user identity icon
+
+@class [template] elements_identicon
+@constructor
+*/
+
+
+Template['elements_identicon'].helpers({
+    /**
+    The current users identity
+
+    @method identity
+    */
+    // 'identity': function(e){
+    //     return this.identity || 'hj'; // hj creates a nice icon with person
+    // }
+});
