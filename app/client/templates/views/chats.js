51cc1a914235d7f259710c4771694b838f684b9f
Fabian Vogelsteller <fabian@frozeman.de>
2015-02-11 11:44:13 +0100

    moved message format helper

--- app/client/templates/views/chats.js
+++ app/client/templates/views/chats.js
@@ -132,56 +132,6 @@ Template['views_chats'].helpers({
         }
     },
     /**
-    Super duper format message helper.
-
-    Strips HTML
-
-    @method (message)
-    */
-    'message': function(){
-        var text = this.message || '';
-
-
-        // add prettyprint
-        // var template = Template.instance();
-        // if(template.view.isRendered) {
-            
-        //     Tracker.afterFlush(function(){
-        //         console.log(template.$('code')[0]);
-        //         template.$('code').wrap('<pre class="prettyprint">');
-        //         prettyPrint();
-        //     });
-        // }
-
-        // make sure not existing values are not Spacebars.kw
-        // if(stripHtml instanceof Spacebars.kw)
-        //     stripHtml = false;
-
-        if(_.isString(text)) {
-
-            text = _.stripTags(text); // stripTags or escapeHTML?
-
-            // parse hashtags and add a target="_blank" to links
-            return text
-                // .replace(/\n+/g,'<br>')
-                // this regex finds every link http://mylink.de, www.mylink.de and even mylink.de
-                // Thanks to Android: http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/2.0_r1/android/text/util/Regex.java#Regex.0WEB_URL_PATTERN
-                .replace(/((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi, '<a href="http://$1" target="_blank">$1</a>')
-                .replace("\n",'<br>')
-                .replace(/http\:\/\/http\:\/\//g,'http://')
-                .replace(/http\:\/\/https\:\/\//g, 'https://')
-                .replace(/(\#[\w]+)/g, '<a href="http://twitter.com/$1">$1</a>')
-                .replace(/^(\@[\w]+)/g, ' <a href="http://twitter.com/$1">$1</a>')
-                .replace(/ (\@[\w]+)/g, ' <a href="http://twitter.com/$1">$1</a>')
-                .replace(/<a href\=\"http\:\/\/twitter\.com/g,'<a target="_blank" href="http://twitter.com');
-
-        } else if(_.isFinite(text)) {
-            return text;
-        } else {
-            return null;
-        }
-    },
-    /**
     Show "more messages" button, if there are more than the current visible ones;
 
     @method (showMoreButton)
